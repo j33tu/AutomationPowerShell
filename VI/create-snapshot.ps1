559dc670c4d647b3d23a8d599c158a98239eab55
Jitendra Singh <jsingh2@linkedin.biz>
2020-08-04 16:01:08 +0530

    VI

---
+++ VI/create-snapshot.ps1
@@ -0,0 +1,11 @@
+﻿Write-host ("importing the module ") 
+import-module VMware.VimAutomation.Core
+Write-host ("provide Credentials to connect to vCenter") 
+$cred = get-credential
+Connect-viserver "vCenter.vclass.local" -ErrorAction Stop -credential $cred
+$vm = Get-VM -Location "vmfolder"
+$date = Get-Date -Format FileDate
+foreach ($vm in $vm) {
+    new-snapshot -vm $vm -name $date -Description " monthly patches"  -confirm:$false 
+    Write-host ("Created Snapshot $date for Virtual Machine $vm") 
+}
